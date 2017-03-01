ca6bd8ebaedcf36570e43cf118ad51d8bd52af19
takatsuna <y-takatsuna@outlook.jp>
2017-03-01 15:50:22 +0900

    v1.0
    1st Commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HELLOWORLD
+//
+//  Created by lab on 2017/03/01.
+//  Copyright © 2017年 lab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
