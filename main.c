511442f4cf9985c17ce98d7458ef839496fdbcf1
takatsuna <y-takatsuna@outlook.jp>
2017-03-02 13:18:58 +0900

    TEST-11
    日付追加を対応しました

--- main.c
+++ main.c
@@ -12,5 +12,6 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Good,Evening!\n");
     printf("We are Labo members!\n");
+    printf("２０１７０３０２\n");
     return 0;
 }
