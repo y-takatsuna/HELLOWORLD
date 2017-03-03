35a1af24a47f0f9db08811572b0f4963dca6f3f1
takatsuna <y-takatsuna@outlook.jp>
2017-03-03 16:57:49 +0900

    TEST-16
    修正しました

--- main.c
+++ main.c
@@ -14,7 +14,8 @@ int main(int argc, const char * argv[]) {
     printf("We are Labo members!\n");
     printf("２０１７０３０３\n");
     printf("中原ですが何か\n");
-    printf("高綱ですが何か\n");
+    printf("turutaですが何か\n");
+
 
     return 0;
 }
